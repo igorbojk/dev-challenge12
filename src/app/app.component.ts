182340c04c9cab83816e460e44aec066b6f45330
Igor <igorbojk@gmail.com>
2018-05-08 10:48:47 +0300

    adding save to file and save to json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {saveAs} from 'file-saver/FileSaver';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   isAddingAccord: boolean;
   isEditing: boolean;
   isShowPoints: boolean;
-  isEditingAccords: boolean;
+
+  constructor() {}
 
   saveString() {
     const string = {
@@ -34,6 +36,7 @@ export class AppComponent {
   startAddingAccord() {
     this.isAddingAccord = true;
     this.isEditing = true;
+    console.log(this.accord);
   }
 
   startAddingString() {
@@ -55,6 +58,27 @@ export class AppComponent {
 
   deleteAccord(stringId, index) {
     this.strings.find(i => i.id === stringId).accords[index] = null;
+  }
+
+  saveFileAsJson() {
+    const blob = new Blob([JSON.stringify(this.strings, null, 4)], {type: 'application/json'});
+    saveAs(blob, 'song.json');
+  }
+
+  saveFileAsTxt() {
+    let song = '';
+    this.strings.forEach(string => {
+      string.accords.forEach(accord => {
+        song += accord == null ? ' ' : accord;
+      });
+      song += '\n';
+      string.letters.forEach(letter => {
+        song += letter === '&nbsp;' ? ' ' : letter;
+      });
+      // song += string.letters.join('');
+    });
+    const blob = new Blob([song], {type: 'text/plain;charset=utf-8'});
+    saveAs(blob, 'song.txt');
   }
 
 }
